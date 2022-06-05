60f93ea86ec4ff86081d17aa0e72634dd63eb9ab
bizyogi <yogesh.barot@bizcloudexperts.com>
2022-06-06 02:24:29 +0530

    full load completed

--- handler.js
+++ handler.js
@@ -2,62 +2,73 @@
 
 const AWS = require("aws-sdk");
 const zlib = require('zlib');
-const DOC = require("dynamodb-doc");
 
-AWS.config.update({ region: "us-west-1" });
+AWS.config.update({ region: "us-west-2" });
 
-const awsClient = AWS.DynamoDB();
-const docClient = new DOC.DynamoDB(awsClient);
+const documentClient = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 
 const S3 = new AWS.S3();
 
-const pfunc = (err, data) => {
-  if (err) {
-    console.log(err, err.stack);
-  } else {
-    console.log(data);
-  }
-}
+const unzipDataFromS3AndInsertIntoDynamodb = async (data, err) => {
+  try {
+    if (err == undefined) {
+      zlib.gunzip(data.Body, async (error, buff) => {
+        if (error != null) {
+          console.log(error)
+        } else {
 
-const getLogEvents = (data, err) => {
-  if (err == undefined) {
-    zlib.gunzip(data.Body, (error, buff) => {
-      if (error != null) {
-        console.log(error)
-      }
-      else {
-        const fileData = buff.toString('utf-8');
-        const recordsArray = fileData.split("\n");
+          const fileData = buff.toString('utf-8');
+          const recordsArray = fileData.split("\n");
 
-        for (let i = 0; i < recordsArray.length; i++) {
-          const record = recordsArray[i];
-          console.log("Inserting record: " + record);
+          for (let i = 0; i < recordsArray.length; i++) {
 
-          const params = {
-            Item: record,
-            TableName: "nm-dt-product-feed-dev"
-          };
-          docClient.putItem(params, pfunc);
+            if (!recordsArray[i]) {
+              continue;
+            }
+
+            const record = JSON.parse(recordsArray[i]);
+
+            const params = {
+              Item: record.Item,
+              TableName: "nm-dt-product-feed-dev"
+            };
+            // documentClient.putItem(params, pfunc);
+            await documentClient.putItem(params).promise();
+          }
         }
-      }
-    });
-  } else {
-    console.log(err, err.stack);
+      });
+    } else {
+      console.log(err, err.stack);
+    }
+  } catch (error) {
+    console.log('Error while unzip data', error);
   }
-
-  //Do final bus logic before exiting
 }
 
 
 exports.fullDataLoad = (event) => {
 
-  S3.getObject({
-    Bucket: 'bizdev-account-db-tables',
-    Key: 's3://bizdev-account-db-tables/AWSDynamoDB/01654185216355-429eb4be/data/',
-  }, (err, data) => {
+  try {
+    let s3BucketKeys = [
+      'AWSDynamoDB/01654185216355-429eb4be/data/lgfdcappiu2hlcbwopejneli5e.json.gz',
+      'AWSDynamoDB/01654185216355-429eb4be/data/qqfz5cesdq2i5pxq74uw5kchru.json.gz',
+      'AWSDynamoDB/01654185216355-429eb4be/data/s2sfacp6qq4tjkxbtyqdy6vbqi.json.gz',
+      'AWSDynamoDB/01654185216355-429eb4be/data/unbuj6rhpa765oqk44tzx7g4eq.json.gz'
+    ];
 
-    if (err) getLogEvents(null, err); // an error occurred
-    else getLogEvents(data);       // successful response
+    s3BucketKeys.forEach(element => {
+      S3.getObject({
+        Bucket: 'bizdev-account-db-tables',
+        Key: element
+      }, (err, data) => {
+        console.log(data, 'data from fullDataLoad');
+        if (err) unzipDataFromS3AndInsertIntoDynamodb(null, err); // an error occurred
+        else unzipDataFromS3AndInsertIntoDynamodb(data);       // successful response
+      });
+    });
+    return true
 
-  });
+  } catch (error) {
+    console.log('Error while full data load', error);
+  }
 };
